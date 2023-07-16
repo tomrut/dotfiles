eadb69464fd31eb30dd0e2c7ade0037d8a6665af
tomrut <tomek.rutkowsk@gmail.com>
2023-07-16 18:53:04 +0200

    Added react snippets

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,12 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("1cae4424345f7fe5225724301ef1a793e610ae5a4e23c023076dc334a9eb940a" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" default))
  '(elfeed-feeds
    '("https://notrelated.xyz/rss" "https://static.fsf.org/fsforg/rss/news.xml" "linuxmintnews"))
  '(org-agenda-files
    '("/home/tomek/org/inbox.org.gpg" "/home/tomek/org/gtd.org.gpg" "/home/tomek/org/tickler.org.gpg"))
  '(package-selected-packages
-   '(sdcv osx-org-clock-menubar org-chef encrypt-region elfeed-org))
+   '(react-snippets org-bullets sdcv osx-org-clock-menubar org-chef encrypt-region elfeed-org))
  '(warning-suppress-types
    '((doom-first-file-hook)
      (doom-first-input-hook)
