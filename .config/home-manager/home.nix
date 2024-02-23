bfb3213659efe85401ddafcb02125cec45e39020
tomrut <tomrut@localhost>
2024-02-23 21:14:52 +0100

    Added tmux configuraiton

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -20,14 +20,13 @@
   home.packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
-    pkgs.lynx    
-    pkgs.ripgrep    
+    pkgs.lynx
+    pkgs.ripgrep
     pkgs.gcc
     pkgs.mc
     pkgs.nodejs_21
     pkgs.yt-dlp
     pkgs.keepassxc
-    pkgs.htop
     pkgs.ripgrep
     pkgs.nerdfonts
     pkgs.unzip
@@ -35,6 +34,8 @@
     pkgs.cargo
     pkgs.remind
     pkgs.gnupg
+    pkgs.kitty
+    pkgs.lazygit
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -80,25 +81,32 @@
     enable = true;
 
     bashrcExtra = ''
-        export EDITOR="nvim";
+      export EDITOR="nvim";
+      export TERMINAL="kitty";
     '';
   };
 
+  programs.broot = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
+  programs.btop.enable = true;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   programs.git = {
     enable = true;
-    userName  = "tomrut";
+    userName = "tomrut";
     userEmail = "tomrut@localhost";
-    
+
   };
-  
-  programs.emacs = { 
-      enable = true;
-      extraPackages =  epkgs: [
-        epkgs.org
-      ];
+
+  programs.emacs = {
+    enable = true;
+    extraPackages = epkgs: [
+      epkgs.org
+    ];
   };
 
   home.shellAliases = {
@@ -112,113 +120,179 @@
     nix-direnv.enable = true;
 
   };
-  
-  programs.neovim = 
-  let
-    toLua = str: "lua << EOF\n${str}\nEOF\n";
-    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
-  in
-  {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    extraPackages = with pkgs; [
+
+  programs.neovim =
+    let
+      toLua = str: "lua << EOF\n${str}\nEOF\n";
+      toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+    in
+    {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      extraPackages = with pkgs; [
         tree-sitter
         nodePackages.typescript-language-server
         emmet-ls
         vscode-langservers-extracted
         tailwindcss-language-server
         nodePackages.typescript-language-server
-    ];
+      ];
 
-    plugins = with pkgs.vimPlugins; [
-      
-      {
+      plugins = with pkgs.vimPlugins; [
+
+        {
           plugin = nvim-lspconfig;
           config = toLuaFile ./nvim/plugin/lsp.lua;
-      }
+        }
 
-      (nvim-treesitter.withPlugins (p: [
-        p.tree-sitter-nix
-        p.tree-sitter-bash
-        p.tree-sitter-javascript
-        p.tree-sitter-typescript
-        p.tree-sitter-lua
-        p.tree-sitter-json
-        p.tree-sitter-css
-        p.tree-sitter-html
-        p.tree-sitter-java
-      ]))
-      telescope-nvim
-      telescope-project-nvim
-      lsp-zero-nvim
-      mason-nvim
-      emmet-vim
-      mason-lspconfig-nvim
-      {
+        (nvim-treesitter.withPlugins (p: [
+          p.tree-sitter-nix
+          p.tree-sitter-bash
+          p.tree-sitter-javascript
+          p.tree-sitter-typescript
+          p.tree-sitter-lua
+          p.tree-sitter-json
+          p.tree-sitter-css
+          p.tree-sitter-html
+          p.tree-sitter-java
+        ]))
+        telescope-nvim
+        telescope-project-nvim
+        lsp-zero-nvim
+        mason-nvim
+        emmet-vim
+        mason-lspconfig-nvim
+        {
           plugin = nvim-cmp;
           config = toLuaFile ./nvim/plugin/cmp.lua;
-      }
-      cmp-nvim-lsp
-      nvim-tree-lua
-      luasnip
-      nvim-web-devicons
-      comment-nvim
-      rose-pine
-      harpoon
-      undotree
-      playground
+        }
+        cmp-nvim-lsp
+        nvim-tree-lua
+        luasnip
+        nvim-web-devicons
+        comment-nvim
+        rose-pine
+        harpoon
+        undotree
+        playground
+        lazygit-nvim
+      ];
+
+      extraConfig = builtins.concatStringsSep "\n" [
+        ''
+          let mapleader = "\<Space>"
+          nnoremap <leader>ff <cmd>Telescope find_files<cr>
+          nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+          nnoremap <leader>fb <cmd>Telescope buffers<cr>
+          nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+          nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
+          nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
+          nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
+          nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+
+          nnoremap <C-q> <cmd>quit<cr>
+          nnoremap <C-s> <cmd>w<cr>
+          nnoremap <C-\> <cmd>NvimTreeToggle<cr>
+
+          imap <C-s> <esc>:w<cr>i
+          imap <C-\> <esc>:NvimTreeToggle<cr>i
+        ''
+
+        ''
+          lua <<EOF
+          require("nvim-web-devicons").setup {}
+          require("nvim-tree").setup {}
+          ${lib.strings.fileContents ./remap.lua}
+          ${lib.strings.fileContents ./set.lua}
+          ${lib.strings.fileContents ./nvim/plugin/colors.lua}
+          EOF
+        ''
+
+        ''
+          lua <<EOF
+            require("nvim-tree").setup()
+            require('Comment').setup({
+                ignore = '^$',
+                toggler = {
+                    line = '<leader>cc',
+                    block = '<leader>bc',
+                },
+                opleader = {
+                    line = '<leader>c',
+                    block = '<leader>b',
+                },
+            })
+          EOF
+        ''
+      ];
+
+    };
+
+  programs.tmux = {
+    enable = true;
+    plugins = with pkgs;  [
+      tmuxPlugins.catppuccin
+      tmuxPlugins.sensible
+      tmuxPlugins.vim-tmux-navigator
+      tmuxPlugins.yank
     ];
 
-    extraConfig = builtins.concatStringsSep "\n" [
-      ''
-        let mapleader = "\<Space>"
-        nnoremap <leader>ff <cmd>Telescope find_files<cr>
-        nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-        nnoremap <leader>fb <cmd>Telescope buffers<cr>
-        nnoremap <leader>fh <cmd>Telescope help_tags<cr>
-        nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
-        nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
-        nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
-        nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+    extraConfig = ''
+      set-option -sa terminal-overrides ",xterm*:Tc"
+      set -g mouse on
 
-        nnoremap <C-q> <cmd>quit<cr>
-        nnoremap <C-s> <cmd>w<cr>
-        nnoremap <C-\> <cmd>NvimTreeToggle<cr>
+      unbind C-b
+      set -g prefix C-Space
+      bind C-Space send-prefix
 
-        imap <C-s> <esc>:w<cr>i
-        imap <C-\> <esc>:NvimTreeToggle<cr>i
-      ''
+      # Vim style pane selection
+      bind h select-pane -L
+      bind j select-pane -D 
+      bind k select-pane -U
+      bind l select-pane -R
 
-      ''
-      lua <<EOF
-      require("nvim-web-devicons").setup {}
-      require("nvim-tree").setup {}
-      ${lib.strings.fileContents ./remap.lua}
-      ${lib.strings.fileContents ./set.lua}
-      ${lib.strings.fileContents ./nvim/plugin/colors.lua}
-      EOF
-      ''
+      # Start windows and panes at 1, not 0
+      set -g base-index 1
+      set -g pane-base-index 1
+      set-window-option -g pane-base-index 1
+      set-option -g renumber-windows on
 
-      ''
-      lua <<EOF
-        require("nvim-tree").setup()
-        require('Comment').setup({
-            ignore = '^$',
-            toggler = {
-                line = '<leader>cc',
-                block = '<leader>bc',
-            },
-            opleader = {
-                line = '<leader>c',
-                block = '<leader>b',
-            },
-        })
-      EOF
-      ''
-    ];
+      # Use Alt-arrow keys without prefix key to switch panes
+      bind -n M-Left select-pane -L
+      bind -n M-Right select-pane -R
+      bind -n M-Up select-pane -U
+      bind -n M-Down select-pane -D
 
+      # Shift arrow to switch windows
+      bind -n S-Left  previous-window
+      bind -n S-Right next-window
+
+      # Shift Alt vim keys to switch windows
+      bind -n M-H previous-window
+      bind -n M-L next-window
+
+      set -g @catppuccin_flavour 'mocha'
+
+      set -g @plugin 'tmux-plugins/tpm'
+      set -g @plugin 'tmux-plugins/tmux-sensible'
+      set -g @plugin 'christoomey/vim-tmux-navigator'
+      set -g @plugin 'dreamsofcode-io/catppuccin-tmux'
+      set -g @plugin 'tmux-plugins/tmux-yank'
+
+      run '~/.tmux/plugins/tpm/tpm'
+
+      # set vi-mode
+      set-window-option -g mode-keys vi
+      # keybindings
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
+    '';
   };
 }
