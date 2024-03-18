2c552f68dacef721235b3fb6b13feed6c0c952cb
tomrut <tomrut@localhost>
2024-03-18 21:08:38 +0100

    Nix configuration added

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -173,7 +173,6 @@
   programs.bat.enable = true;
 
   # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
   programs.git = {
     enable = true;
     userName = "tomrut";
