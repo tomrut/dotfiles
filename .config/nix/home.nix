f04e67cf3932bf8c48d5e21daeb907e4d3e15d24
tomrut <tomek.rutkowski@gmail.com>
2024-05-04 18:53:16 +0200

    Added home.nix for non-nixos os

---
+++ .config/nix/home.nix
@@ -0,0 +1,353 @@
+{ config, pkgs, lib, vimUtils, ... }:
+
+#
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "tomek";
+  home.homeDirectory = "/home/tomek";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    pkgs.lynx
+    pkgs.ripgrep
+    pkgs.gcc
+    pkgs.mc
+    pkgs.nodejs_21
+    pkgs.yt-dlp
+    pkgs.keepassxc
+    pkgs.ripgrep
+    pkgs.material-design-icons
+    pkgs.nerdfonts
+    pkgs.unzip
+    pkgs.mpg123
+    pkgs.cargo
+    pkgs.remind
+    pkgs.gnupg
+    pkgs.lazygit
+    pkgs.gxmessage
+    pkgs.jdk21
+    pkgs.maven
+    pkgs.jetbrains.idea-community
+    pkgs.nodePackages.pnpm
+    pkgs.neofetch
+    pkgs.anki
+    pkgs.neomutt
+    pkgs.isync
+    pkgs.pinentry
+    pkgs.notify-desktop
+    pkgs.inkscape
+    pkgs.firefox
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/.screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+    "SDKS/OpenJDK21".source = pkgs.jdk21;
+
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/tomek/etc/profile.d/hm-session-vars.sh
+  #
+
+  programs.btop.enable = true;
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    autocd = true;
+    shellAliases = {
+      mci = "mvn clean install -DskipTests";
+      mcit = "mvn clean install";
+      mcp = "mvn clean package -DskipTests";
+      mcpt = "mvn clean package";
+      lg = "lazygit";
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "random";
+      plugins = [
+        "git"
+        "vi-mode"
+      ];
+    };
+  };
+
+  programs.fzf.enable = true;
+  programs.bat.enable = true;
+
+  # Let Home Manager install and manage itself.
+  programs.git = {
+    enable = true;
+    userName = "tomrut";
+    userEmail = "tomrut@localhost";
+
+  };
+
+  programs.emacs = {
+    enable = true;
+    extraPackages = epkgs: [
+      epkgs.org
+    ];
+  };
+
+  home.shellAliases = {
+    kal = "remind -n1 -c -@ .reminders";
+    kal2 = "remind -n1 -c2 -@ .reminders";
+    lclassic = "mpg123 https://rs101-krk.rmfstream.pl/RMFCLASSIC48";
+    lsoundtracks = "mpg123 https://kathy.torontocast.com:1190/";
+  };
+
+  programs.neovim =
+    let
+      toLua = str: "lua << EOF\n${str}\nEOF\n";
+      toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+    in
+    {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      extraPackages = with pkgs; [
+        tree-sitter
+        nodePackages.typescript-language-server
+        emmet-ls
+        vscode-langservers-extracted
+        tailwindcss-language-server
+        nodePackages.typescript-language-server
+      ];
+
+      plugins = with pkgs.vimPlugins; [
+
+        {
+          plugin = nvim-lspconfig;
+          config = toLuaFile ./nvim/plugin/lsp.lua;
+        }
+
+        (nvim-treesitter.withPlugins (p: [
+          p.tree-sitter-nix
+          p.tree-sitter-bash
+          p.tree-sitter-javascript
+          p.tree-sitter-typescript
+          p.tree-sitter-lua
+          p.tree-sitter-json
+          p.tree-sitter-css
+          p.tree-sitter-html
+          p.tree-sitter-java
+        ]))
+        telescope-nvim
+        telescope-project-nvim
+        lsp-zero-nvim
+        mason-nvim
+        emmet-vim
+        mason-lspconfig-nvim
+        {
+          plugin = nvim-cmp;
+          config = toLuaFile ./nvim/plugin/cmp.lua;
+        }
+        cmp-nvim-lsp
+        nvim-tree-lua
+        luasnip
+        nvim-web-devicons
+        comment-nvim
+        rose-pine
+        harpoon
+        undotree
+        playground
+        lazygit-nvim
+      ];
+
+      extraConfig = builtins.concatStringsSep "\n" [
+        ''
+          let mapleader = "\<Space>"
+          nnoremap <leader>ff <cmd>Telescope find_files<cr>
+          nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+          nnoremap <leader>fb <cmd>Telescope buffers<cr>
+          nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+          nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
+          nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
+          nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
+          nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+
+          nnoremap <C-q> <cmd>quit<cr>
+          nnoremap <C-s> <cmd>w<cr>
+          nnoremap <C-\> <cmd>NvimTreeToggle<cr>
+
+          imap <C-s> <esc>:w<cr>i
+          imap <C-\> <esc>:NvimTreeToggle<cr>i
+        ''
+
+        ''
+          lua <<EOF
+          require("nvim-web-devicons").setup {}
+          require("nvim-tree").setup {}
+          ${lib.strings.fileContents ./remap.lua}
+          ${lib.strings.fileContents ./set.lua}
+          ${lib.strings.fileContents ./nvim/plugin/colors.lua}
+          EOF
+        ''
+
+        ''
+          lua <<EOF
+            require("nvim-tree").setup()
+            require('Comment').setup({
+                ignore = '^$',
+                toggler = {
+                    line = '<leader>cc',
+                    block = '<leader>bc',
+                },
+                opleader = {
+                    line = '<leader>c',
+                    block = '<leader>b',
+                },
+            })
+          EOF
+        ''
+      ];
+
+    };
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      dracula-theme.theme-dracula
+      vscodevim.vim
+      yzhang.markdown-all-in-one
+    ];
+  };
+
+
+  programs.tmux = {
+    enable = true;
+    plugins = with pkgs;  [
+      tmuxPlugins.sensible
+      tmuxPlugins.vim-tmux-navigator
+      tmuxPlugins.yank
+
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_window_status_enable "yes"
+          set -g @catppuccin_flavour 'frappe'
+          set -g @catppuccin_window_left_separator ""
+          set -g @catppuccin_window_right_separator " "
+          set -g @catppuccin_window_middle_separator " █"
+          set -g @catppuccin_window_number_position "right"
+
+          set -g @catppuccin_window_default_fill "number"
+          set -g @catppuccin_window_default_text "#W"
+
+          set -g @catppuccin_window_current_fill "number"
+          set -g @catppuccin_window_current_text "#W"
+
+          set -g @catppuccin_status_modules_right "directory user host session"
+          set -g @catppuccin_status_left_separator  " "
+          set -g @catppuccin_status_right_separator ""
+          set -g @catppuccin_status_right_separator_inverse "no"
+          set -g @catppuccin_status_fill "icon"
+          set -g @catppuccin_status_connect_separator "no"
+          set -g @catppuccin_directory_text "#{pane_current_path}" 
+        '';
+      }
+    ];
+
+    extraConfig = ''
+      set-option -sa terminal-overrides ",xterm*:Tc"
+      set -g mouse on
+
+      unbind C-b
+      set -g prefix C-Space
+      bind C-Space send-prefix
+
+      # Vim style pane selection
+      bind h select-pane -L
+      bind j select-pane -D 
+      bind k select-pane -U
+      bind l select-pane -R
+
+      # Start windows and panes at 1, not 0
+      set -g base-index 1
+      set -g pane-base-index 1
+      set-window-option -g pane-base-index 1
+      set-option -g renumber-windows on
+
+      # Use Alt-arrow keys without prefix key to switch panes
+      bind -n M-Left select-pane -L
+      bind -n M-Right select-pane -R
+      bind -n M-Up select-pane -U
+      bind -n M-Down select-pane -D
+
+      # Shift arrow to switch windows
+      bind -n S-Left  previous-window
+      bind -n S-Right next-window
+
+      # Shift Alt vim keys to switch windows
+      bind -n M-H previous-window
+      bind -n M-L next-window
+
+      # set vi-mode
+      set-window-option -g mode-keys vi
+      # keybindings
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
+
+      set -g default-shell /home/tomek/.nix-profile/bin/zsh
+    '';
+  };
+
+  programs.zathura = {
+    enable = true;
+  };
+  
+  programs.home-manager.enable = true;
+}
