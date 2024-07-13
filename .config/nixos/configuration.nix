edfaa5eb3c5d0d136ad39d3dffa313de8d176447
tomrut <tomrut@localhost>
2024-07-13 20:19:28 +0200

    Changed greeter to gtk

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -55,7 +55,15 @@
   services.xserver.enable = true;
 
   # Enable the Cinnamon Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.lightdm = {
+    enable = true;
+    greeters.slick.enable = false;
+    greeters.gtk = {
+        enable = true;
+    };
+  };
+
+
   services.xserver.desktopManager.cinnamon.enable = true;
 
   # Configure keymap in X11
