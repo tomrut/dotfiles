773bb433e5ef0a51b882c9042da6f9693d9d80c1
tomrut <tomrut@localhost>
2024-07-14 13:58:18 +0200

    Added nvim mapping to open file in browser F12

--- .config/nixos/home-manager/remap.lua
+++ .config/nixos/home-manager/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<C-q>", vim.cmd.quit)
+vim.keymap.set("n", "<F12>", ':silent !firefox %<CR>')
+vim.keymap.set("i", "<F12>", '<Esc>:silent !firefox %<CR>')
 vim.keymap.set("n", "<C-s>", vim.cmd.w)
 vim.keymap.set("i", "<C-s>", '<Esc>:w<CR>i')
 vim.keymap.set("i", "<C-\\>", '<Esc>:NvimTreeToggle<CR>i')
