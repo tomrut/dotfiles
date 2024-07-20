9248d8529a090f9046dd3898261c5fd0b7fd2662
tomrut <tomrut@localhost>
2024-07-20 11:31:13 +0200

    Disable window picker in nvim-tree since is not working properly.

--- .config/nixos/home-manager/nvim/init.lua
+++ .config/nixos/home-manager/nvim/init.lua
@@ -1,5 +1,13 @@
 require("nvim-web-devicons").setup {}
-require("nvim-tree").setup {}
+require("nvim-tree").setup ({
+    actions = {
+        open_file = {
+            window_picker = {
+                enable = false 
+            }
+        }
+    }
+})
 require('lualine').setup({
   options = { theme = 'dracula' }
 })
