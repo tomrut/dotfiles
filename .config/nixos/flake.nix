4c3afe72e12acc92e2b9b3330079c28940780aed
tomrut <tomrut@localhost>
2024-12-26 20:36:31 +0100

    Flake cleanup

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -8,7 +8,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager,... }@inputs: {
+  outputs = { nixpkgs, home-manager,... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
