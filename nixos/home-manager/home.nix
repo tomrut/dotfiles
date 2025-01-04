3156abfea4fdae9ec7d74fe9ff55c6afe4d45db8
tomrut <tomrut@localhost>
2025-01-04 20:04:34 +0100

    Link sway configuration

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -141,6 +141,9 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/.screenrc;
+    ".config/sway/config".source = sway/config;
+    ".config/waybar/config".source = waybar/config;
+    ".config/waybar/style.css".source = waybar/style.css;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
