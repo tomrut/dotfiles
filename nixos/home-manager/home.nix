79d93436962689807cf92995f2ce0e47e8ae28ea
tomrut <tomrut@localhost>
2025-02-01 20:03:05 +0100

    Removed some redundant programs and switch to use subset of nerdfonts

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -99,19 +99,16 @@
     pkgs.keepassxc
     pkgs.ripgrep
     pkgs.material-design-icons
-    pkgs.nerdfonts
-    #(pkgs.nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono"  "Monoid" "DroidSansMono" "MPlus" "NerdFontsSymbolsOnly" ]; })
+    pkgs.fira-code-nerdfont
     pkgs.unzip
     pkgs.mpg123
     pkgs.cargo
     pkgs.remind
     pkgs.gnupg
-    pkgs.gxmessage
     pkgs.jdk21
     pkgs.maven
     pkgs.jetbrains.idea-community
     pkgs.nodePackages.pnpm
-    pkgs.neofetch
     pkgs.anki
     pkgs.neomutt
     pkgs.isync
@@ -120,15 +117,12 @@
     pkgs.flameshot
     pkgs.lazygit
     pkgs.fd
-    pkgs.md4c
-    pkgs.lua-language-server
     pkgs.home-manager
     pkgs.yubioath-flutter
     pkgs.nixfmt-rfc-style
     pkgs.nvd
     pkgs.firefox
     pkgs.feh
-    pkgs.jq
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -290,55 +284,6 @@
     enableBashIntegration = true; # see note on other shells below
     nix-direnv.enable = true;
 
-  };
-
-  programs.tmux = {
-    enable = true;
-
-    extraConfig = ''
-      set-option -sa terminal-overrides ",xterm*:Tc"
-      set -g mouse on
-
-      unbind C-b
-      set -g prefix C-Space
-      bind C-Space send-prefix
-
-      # Vim style pane selection
-      bind h select-pane -L
-      bind j select-pane -D
-      bind k select-pane -U
-      bind l select-pane -R
-
-      # Start windows and panes at 1, not 0
-      set -g base-index 1
-      set -g pane-base-index 1
-      set-window-option -g pane-base-index 1
-      set-option -g renumber-windows on
-
-      # Use Alt-arrow keys without prefix key to switch panes
-      bind -n M-Left select-pane -L
-      bind -n M-Right select-pane -R
-      bind -n M-Up select-pane -U
-      bind -n M-Down select-pane -D
-
-      # Shift arrow to switch windows
-      bind -n S-Left  previous-window
-      bind -n S-Right next-window
-
-      # Shift Alt vim keys to switch windows
-      bind -n M-H previous-window
-      bind -n M-L next-window
-
-      # set vi-mode
-      set-window-option -g mode-keys vi
-      # keybindings
-      bind-key -T copy-mode-vi v send-keys -X begin-selection
-      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
-
-      bind '"' split-window -v -c "#{pane_current_path}"
-      bind % split-window -h -c "#{pane_current_path}"
-    '';
   };
 
   programs.zathura = {
