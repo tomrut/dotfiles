a15fc3564e8abe09c15e3c34f2e0f1d27cff11f1
tomrut <tomrut@localhost>
2025-02-02 12:00:43 +0100

    Add dbus env update

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -29,6 +29,7 @@
             export WAYLAND_DISPLAY=wayland-1
             export XDG_SESSION_TYPE=wayland
             eval `${pkgs.dbus}/bin/dbus-launch --sh-syntax`
+            ${pkgs.dbus}/bin/dbus-update-activation-environment
             export DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS
             text=$(${pkgs.remind}/bin/rem)
             ${pkgs.libnotify}/bin/notify-send -w "today's reminders" "$text"
