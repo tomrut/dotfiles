1497efa8adf8de97eb0e7dbc9fa2a9e00491b70e
tomrut <tomrut@localhost>
2025-07-18 20:55:14 +0200

    Added script for listening to the radion

---
+++ bin/listen-radio.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+last=""
+
+mpg123 "$1" 2>&1 |
+  grep --line-buffered "ICY-META" |
+  sed -u -n "s/.*StreamTitle='\([^']*\)'.*/\1/p" |
+  while read -r title; do
+    if [[ -n "$title" && "$title" != "$last" ]]; then
+      notify-send "Now Playing" "$title"
+      last="$title"
+    fi
+  done
