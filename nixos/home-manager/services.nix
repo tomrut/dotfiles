cb8365bd01678e913686ff346b12793447b87cbe
tomrut <tomrut@localhost>
2025-08-15 22:41:41 +0200

    Added bindsym for umounting devices

--- nixos/home-manager/services.nix
+++ nixos/home-manager/services.nix
@@ -105,12 +105,6 @@
   services.udiskie = {
     enable = true;
     settings = {
-      # workaround for
-      # https://github.com/nix-community/home-manager/issues/632
-      program_options = {
-        # replace with your favorite file manager
-        file_manager = "${pkgs.foot}/bin/foot -T ranger -e ${pkgs.ranger}/bin/ranger";
-      };
     };
   };
 }
