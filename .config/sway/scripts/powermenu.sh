516f4b9aa675fe9d303e59485a56ef84d958b3c3
tomrut <tomrut@localhost>
2025-10-17 19:08:48 +0200

    polish waybar and fuzzel menus

--- .config/sway/scripts/powermenu.sh
+++ .config/sway/scripts/powermenu.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 options="⏻   PowerOff\n   Reboot\n󰤄   Sleep\n󰌾   Lock\n󰍃   Logout"
 
-choice=$(echo -e "$options" | fuzzel --dmenu --prompt="Select Power Option: ")
+choice=$(echo -e "$options" | fuzzel -w 15 -l 6 -a top-right --dmenu --prompt="Select Power Option: ")
 
 case "$choice" in
-    "⏻   PowerOff")
-        systemctl poweroff
-        ;;
-    "   Reboot")
-        systemctl reboot
-        ;;
-    "󰤄   Sleep")
-        systemctl suspend
-        ;;
-    "󰌾   Lock")
-        swaylock -f -c 000000 
-        ;;
-    "󰍃   Logout")
-        swaymsg exit
-        ;;
-    *)
-        echo "No valid option selected."
-        ;;
+"⏻   PowerOff")
+  systemctl poweroff
+  ;;
+"   Reboot")
+  systemctl reboot
+  ;;
+"󰤄   Sleep")
+  systemctl suspend
+  ;;
+"󰌾   Lock")
+  swaylock -f -c 000000
+  ;;
+"󰍃   Logout")
+  swaymsg exit
+  ;;
+*)
+  echo "No valid option selected."
+  ;;
 esac
-
