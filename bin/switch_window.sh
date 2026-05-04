c03c730e12d978f355ea813d75a58984abf99ec3
tomrut <tomrut@localhost>
2026-05-04 19:19:44 +0200

    Add window switching tool - enh

--- bin/switch_window.sh
+++ bin/switch_window.sh
@@ -3,7 +3,7 @@
 windows=$(swaymsg -t get_tree | jq -r '.nodes[1].nodes[].nodes[] | .. | (.id|tostring) + " " + .name?' | grep -e "[0-9]* .")
 
 # Select window with rofi
-selected=$(echo "$windows" | wofi -i | awk '{print $1}')
+selected=$(echo "$windows" | wofi -W 60% -d -i --hide-scroll -p "Switch window:" | awk '{print $1}')
 
 # Tell sway to focus said window
 swaymsg [con_id="$selected"] focus
