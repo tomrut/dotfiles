09115fa4dc9d81ad6381dff533bb43b50efa4dfe
tomrut <tomrut@localhost>
2026-05-04 19:04:52 +0200

    Add window switching tool

---
+++ bin/switch_window.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Get available windows
+windows=$(swaymsg -t get_tree | jq -r '.nodes[1].nodes[].nodes[] | .. | (.id|tostring) + " " + .name?' | grep -e "[0-9]* .")
+
+# Select window with rofi
+selected=$(echo "$windows" | wofi -i | awk '{print $1}')
+
+# Tell sway to focus said window
+swaymsg [con_id="$selected"] focus
