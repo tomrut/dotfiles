a0b19005be602e44dd05a43eab7c2db1552f493e
tomrut <tomrut@localhost>
2026-05-15 21:54:17 +0200

    Added icons to bar_status command for swaybar

--- bin/bar_status.sh
+++ bin/bar_status.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-date=$(date +%d-%B-%Y\ %H:%M)
+date=$(date +%d\ %B\ %Y\ ❙\ 🕐\ %H:%M)
 bat0=$(cat /sys/class/power_supply/BAT0/capacity)
 bat1=$(cat /sys/class/power_supply/BAT1/capacity)
-cpu_temp="$(awk '{x += $1} END{ printf "%.2f", x / NR / 1000}' /sys/class/thermal/thermal_zone*/temp)°C"
+cpu_temp="$(awk '{x += $1} END{ printf "%.0f", x / NR / 1000}' /sys/class/thermal/thermal_zone*/temp)°C"
 mem=$(free -m | grep Pami | awk '{print ($3/$2)*100}')
 mem_rounded=$(printf "%.0f\n" $mem)
 cpu_util=$(vmstat 1 2 | tail -1 | awk '{print 100 - $15""}')
+sound_volume=$(pulsemixer --get-volume | awk '{print $2}')
 
-echo "CPU: ${cpu_util}%  MEM: $mem_rounded%  TEMP: ${cpu_temp}  BAT0: ${bat0}%  BAT1: ${bat1}%  ${date}"
+echo "❙ 💎${cpu_util}% ❙  $mem_rounded% ❙ ⚡${cpu_temp} ❙ 🎧${sound_volume}% ❙ 🔋${bat0}% ❙ 🔋${bat1}% ❙ ${date} ❙"
