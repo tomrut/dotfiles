c7778987f14e3fa0f9a2ac359ff2c836bea1eb31
tomrut <tomrut@localhost>
2026-05-17 11:21:42 +0200

    Impr status in swaybar

--- bin/bar_status.sh
+++ bin/bar_status.sh
@@ -27,7 +27,7 @@ battery() {
 bat0=$(battery 0)
 bat1=$(battery 1)
 cpu_temp="$(awk '{x += $1} END{ printf "%.0f", x / NR / 1000}' /sys/class/thermal/thermal_zone*/temp)°C"
-mem=$(free -m | grep Pami | awk '{print ($3/$2)*100}')
+mem=$(free -m | grep -E "Pami|Mem" | awk '{print ($3/$2)*100}')
 mem_rounded=$(printf "%.0f" "${mem/./,}")
 cpu_util=$(vmstat 1 2 | tail -1 | awk '{print 100 - $15""}')
 sound_volume=$(pulsemixer --get-volume | awk '{print $2}')
