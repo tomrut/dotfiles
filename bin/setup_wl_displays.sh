5f420e185043813fab1100d9223d9d010566045b
tomrut <tomrut@localhost>
2026-05-18 19:55:24 +0200

    Adjust monitor order for sway on desktop

--- bin/setup_wl_displays.sh
+++ bin/setup_wl_displays.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-outputs_num=$(swaymsg -t get_outputs  | grep name | wc -l)
+outputs_num=$(swaymsg -t get_outputs | grep name | wc -l)
 echo "outputsnum: $outputs_num"
 [[ $outputs_num == 3 ]] && swaymsg output LVDS-1 disable
-# if [[ $outputs_num == 2 ]]; then 
+
+output_laptop=$(swaymsg -t get_outputs | grep LVDS-1 | wc -l)
+if [[ $output_laptop == 0 ]]; then
+  echo "ll $output_laptop"
+  swaymsg output DP-4 pos 0 0
+  swaymsg output DP-5 pos 1920 0
+fi
+# if [[ $outputs_num == 2 ]]; then
 #   export second_output=$(swaymsg -t  get_outputs|jq -r '.[] | select(.name != "LVDS-1") | .name')
 #   echo "set \$output1 $second_output" > ~/.config/sway/outputs
 # else
@@ -10,4 +17,3 @@ echo "outputsnum: $outputs_num"
 # fi
 #
 # sync
-
